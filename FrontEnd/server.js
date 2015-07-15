260dc47a1b0b691b4b14fcd0bb002ee346b74e55
Sean Ross <sfortunato@yondu.com>
2015-07-15 17:56:20 +0800

    add header and included it to the main layout

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -31,7 +31,7 @@ app.use(stylus.middleware({
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'jade');
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/media/favicon.ico'));
 app.use(logger('dev'));
 app.use(methodOverride());
 app.use(session({ resave: true,
